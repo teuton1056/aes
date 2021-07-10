8ec75813dac3c426ca7b0acebb34a88e77e007ff
teuton1056 <kingswarrior08@gmail.com>
2021-07-10 14:20:27 -0500

    basic conversion done

--- convert_to_conll.py
+++ convert_to_conll.py
@@ -1,19 +1,40 @@
 from sklearn.base import MetaEstimatorMixin
 import aes_statistics 
 import json 
-import conllu
+from conllu import TokenList
+import html
+import time
 
-with open('upos_aes.json','r') as fp:
-	pos_conversion_table = json.load(fp)
-
-pos_tags = set()
-data = aes_statistics.load_data_better()
+def load_conversion_tables():
+	with open('upos_aes.json','r') as fp:
+		upos_conversion_table = json.load(fp)
+	return upos_conversion_table
 
 def convert_corpus(data):
+	t = time.time()
+	uct = load_conversion_tables()
+	total = 0
+	corpus = []
 	for sentence in data:
-		convert_sentence(sentence)
+		if check_sentence(sentence):
+			total += 1
+			s = convert_sentence(sentence,uct)
+			corpus.append(s)
+	print(f"A total of {total} sentences have been converted")
+	with open('corpus.conllu','w',encoding='utf-8') as fp:
+		for sentence in corpus:
+			fp.write(sentence.serialize())
+	print("All Sentence Written")
+	print(f"That took a total of {time.time() - t} seconds.")
+
+def check_sentence(aes_sentence):
+	for token in aes_sentence['token']:
+		if 'hiero_unicode' not in token.keys():
+			return False
+	return True
 
 def convert_sentence(aes_sentence,upos_conversion_table):
+	new_tokens = []
 	# get sentence meta_data:
 	metadata = {}
 	metadata['sentid'] = aes_sentence['sentence_id']
@@ -22,22 +43,115 @@ def convert_sentence(aes_sentence,upos_conversion_table):
 	metadata['owner'] = aes_sentence['owner']
 	try:
 		metadata['text_de'] = aes_sentence['sentence_translation']
-	except:
+	except Exception:
 		print('No Translation')
 	try:
 		metadata['findspot'] = aes_sentence['findspot']
-	except:
+	except Exception:
 		print('No Findspot')
 	try:
 		metadata['date'] = aes_sentence['date']
-	except:
+	except Exception:
 		print('No Date')
 	# get token data
+	id_int_number = 1
+	tokens = []
 	for token in aes_sentence['token']:
+		this_token = {}
+		this_token['id'] = id_int_number 
+		id_int_number += 1
+		# form and lemma
+		this_token['form'] = html.unescape(token['hiero_unicode'])
+		try:
+			this_token['lemma'] = token['lemmaID']
+		except:
+			this_token['lemma'] = '_'
+
 		# upos and xpos
 		if 'pos' in token.keys():
 			xpos = token['pos']
 			try:
 				upos = upos_conversion_table[xpos]
-			except:
+			except Exception:
 				upos = '_'
+		else:
+			upos = '_'
+			xpos = '_'
+		this_token['upos'] = upos
+		this_token['xpos'] = xpos
+
+		# try to do some features for nouns
+		features = {} 
+		number = {
+			'singular':'Sing',
+			'dual':'Dual',
+			'plural':'Plur'
+		}
+		if 'numerus' in token.keys():
+			try:
+				features['Number'] = number[token['numerus']]
+			except:
+				pass
+		if 'status' in token.keys():
+			if token['status'] == 'st_constructus':
+				features['Status'] = 'Cons'
+			elif token['status'] == 'st_absolutus':
+				features['Status'] = 'Abs'
+			elif token['status'] == 'st_pronominalis':
+				features['Status'] = 'Pron'
+		gender = {
+			'masculine':'Masc',
+			'feminine':'Fem',
+			'commonGender':'Com'
+		}
+		if 'genus' in token.keys():
+			features['Gender'] = gender[token['genus']]
+		
+		# verb stuff
+		voice = {'active':'Act','passive':'Pass'}
+		if 'voice' in token.keys():
+			features['Voice'] = voice[token['voice']]
+		if 'morphology' in token.keys():
+			if 'n-morpheme'	== token['morphology']:
+				features['Aspect'] = 'Perf'
+		if 'inflection' in token.keys():
+			if token['inflection'] == 'infintive':
+				features['VerbForm'] = 'Inf'
+			elif token['inflection'] == 'pseudoParticiple':
+				features['VerbForm'] = 'Conv'
+			elif token['inflection'] == 'participle':
+				features['VerbForm'] = 'Part'
+			else:
+				features['VerbForm'] = 'Fin'
+			if token['inflection'] == 'suffixConjugation':
+				features['Mood'] = 'Ind'
+			if token['inflection'] == 'imperative':
+				features['Mood'] = 'Imp'
+	
+		# other pos
+		if 'pronoun' in token.keys():
+			if token['pronoun'] == 'personal_pronoun':
+				features['PronType'] = 'Prs'
+			elif token['pronoun'] == 'demonstrative_pronoun':
+				features['PronType'] = 'Dem'
+			elif token['pronoun'] == 'interrogative_pronoun':
+				features['PronType'] = 'Int'
+			elif token['pronoun'] == 'relative_pronoun':
+				features['PronType'] = 'Rel'
+		this_token['features'] = features
+		this_token['head'] = '_'
+		this_token['deprel'] = '_'
+		this_token['deps'] = '_'
+		this_token['misc'] = '_'
+		new_tokens.append(this_token)
+	new_sentence = TokenList(new_tokens)
+	new_sentence.metadata = metadata
+	return new_sentence
+
+
+def main():
+	data = aes_statistics.load_data_better()
+	convert_corpus(data)
+
+if __name__ == '__main__':
+	main()
